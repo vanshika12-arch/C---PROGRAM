3a02dbbbbcb25495a336c83024deb9045db97798
vanshika singh <itzkhushisingh12@gmail.com>
2026-04-01 19:44:23 +0530

    C program for entering name using scanf printf

---
+++ NAMESCAN.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[10];
+clrscr();
+printf("enter your name:");
+scanf("%s",a);
+printf("your name is %s %s\n",a,"Singh");
+getch();
+}
